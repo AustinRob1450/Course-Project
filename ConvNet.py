a635518152350df273dcb2511c5ac228b1186cd0
AustinRob1450 <53237132+AustinRob1450@users.noreply.github.com>
2021-11-08 09:49:00 -0500

    Add files via upload

--- ConvNet.py
+++ ConvNet.py
@@ -11,6 +11,7 @@ from torchvision import datasets, transforms, utils
 from torch.utils.tensorboard import SummaryWriter
 import argparse
 import numpy as np 
+import matplotlib.pyplot as plt
 
 # @misc{mulitdigitmnist,
 #   author = {Sun, Shao-Hua},
@@ -32,24 +33,20 @@ class ConvNet(nn.Module):
         # 3. Shape of your kernel
         
         # 1
-        self.fc1 = nn.Linear(28*28, 100)
-        self.fc2 = nn.Linear(100, 10)
-        # 2, 3, 4
-        self.conv1 = nn.Conv2d(1, 40, 5, padding=2)
+        # self.fc1 = nn.Linear(28*28, 100)
+        self.fc1 = nn.Linear(64*64, 100)
+        self.fc1a = nn.Linear(1000, 10)
+        # self.fc1b = nn.Linear(100, 10)
+        
+        # 2
+        self.conv = nn.Conv2d(3, 80, 5, padding=2)
         self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        self.conv2 = nn.Conv2d(40, 80, 5, padding=2)
+        self.conv2 = nn.Conv2d(80, 160, 5, padding=2)
         
         # 2 max pool layers --> Half 28 twice
-        self.newfc = nn.Linear(80*7*7, 100)
+        self.newfc = nn.Linear(160*16*16, 100)
         self.newfc2 = nn.Linear(100, 10)
-        
-        # 5
-        self.conv1 = nn.Conv2d(1, 40, 5, padding=2)
-        self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-        self.conv2 = nn.Conv2d(40, 80, 5, padding=2)
-        self.newfc = nn.Linear(80*7*7, 1000)
-        self.newfc2 = nn.Linear(1000, 10)
-
+  
 
         # Activations
         self.sigmoid = nn.Sigmoid()
@@ -75,13 +72,6 @@ class ConvNet(nn.Module):
             print("Invalid mode ", mode, "selected. Select between 1-5")
             exit(0)
 
-    def numFlatFeatures(self, x):
-      size = x.size()[1:]
-      num_feat = 1
-      for s in size:
-        num_feat *= s
-      return num_feat
-
 
     # Baseline model. step 1
     def model_1(self, X):
@@ -91,14 +81,17 @@ class ConvNet(nn.Module):
         # ----------------- YOUR CODE HERE ----------------------
         #
         # print("MODEL 1")
-        # print(X.shape)
-        # X = X.view(-1, 28*28)
-        # print(X.shape)
+        print("~~~~~~", X.shape)
+        print("++++", X.shape[1])
+        
+        X = X.view(X.shape[0], 64*64)
+        print("~~~~~~", X.shape)
 
         X = self.fc1(X)
         X = self.sigmoid(X)
-        X = self.fc2(X)
-        
+        X = self.fc1a(X)
+
+
         fcl = X
         # Uncomment the following return stmt once method implementation is done.
         return  fcl
@@ -112,94 +105,27 @@ class ConvNet(nn.Module):
         #
         # ----------------- YOUR CODE HERE ----------------------
         #
-        # print("SHAPE", X.shape)
-        # print("0 shape:", X.shape[0])
-        print("MODEL 2")
-        X = self.sigmoid(self.conv1(X))
-        X = self.pool(X)
+        # self.fc1 = nn.Linear(64*64, 100)
+        # self.fc1a = nn.Linear(100, 10)
+        # # 2, 3, 4
+        # self.conv1 = nn.Conv2d(3, 80, 5, padding=2)
+        # self.pool = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
+        # self.conv2 = nn.Conv2d(80, 160, 5, padding=2)
+
+        X = self.sigmoid(self.conv(X))
         X = self.sigmoid(self.conv2(X))
-        X = self.pool(X)
-        X = X.reshape(X.shape[0], -1)
-
-        X = self.newfc(X)
-
-        fcl = X
-        # Uncomment the following return stmt once method implementation is done.
-        return  fcl
-        # Delete line return NotImplementedError() once method is implemented.
-        # return NotImplementedError()
-
-    # Replace sigmoid with ReLU.
-    def model_3(self, X):
-        # ======================================================================
-        # Two convolutional layers + one fully connected layer, with ReLU.
-        #
-        # ----------------- YOUR CODE HERE ----------------------
-        #
-        # print("MODEL 3")
-        X = self.relu(self.conv1(X))
-        X = self.pool(X)
-        X = self.relu(self.conv2(X))
-        X = self.pool(X)
-        X = X.reshape(X.shape[0], -1)
-
-        X = self.newfc(X)
-
-        fcl = X
-
-        # Uncomment the following return stmt once method implementation is done.
-        return  fcl
-        # Delete line return NotImplementedError() once method is implemented.
-        # return NotImplementedError()
-
-    # Add one extra fully connected layer.
-    def model_4(self, X):
-        # ======================================================================
-        # Two convolutional layers + two fully connected layers, with ReLU.
-        #
-        # ----------------- YOUR CODE HERE ----------------------
-        #
-        # print("MODEL 4")
-        X = self.relu(self.conv1(X))
-        X = self.pool(X)
-        X = self.relu(self.conv2(X))
-        X = self.pool(X)
-        X = X.reshape(X.shape[0], -1)
+        print("Shape~~~~~~~~~~~~~~~~~~~~~~", X.shape)
         X = self.newfc(X)
         X = self.newfc2(X)
-
-        fcl = X
-
-        # Uncomment the following return stmt once method implementation is done.
-        return  fcl
-        # Delete line return NotImplementedError() once method is implemented.
-        # return NotImplementedError()
-
-    # Use Dropout now.
-    def model_5(self, X):
-        # ======================================================================
-        # Two convolutional layers + two fully connected layers, with ReLU.
-        # and  + Dropout.
-        #
-        # ----------------- YOUR CODE HERE ----------------------
-        #
-        # print("MODEL 5")
-        X = self.relu(self.conv1(X))
-        X = self.pool(X)
-        X = self.relu(self.conv2(X))
-        X = self.pool(X)
         X = X.reshape(X.shape[0], -1)
-        X = self.newfc(X)
-        X = self.dropout(X)
-        X = self.newfc2(X)
-        X = self.dropout(X)
 
         fcl = X
         # Uncomment the following return stmt once method implementation is done.
         return  fcl
         # Delete line return NotImplementedError() once method is implemented.
         # return NotImplementedError()
-    
+
+
     
 
 
